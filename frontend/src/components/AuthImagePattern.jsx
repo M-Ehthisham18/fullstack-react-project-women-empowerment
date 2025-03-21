cc7206bd39aeb113e8fa394a6474923e76c877b6
M-Ehthisham18 <ehthishamulhaq18@gamil.com>
2025-03-21 19:45:39 +0530

    login page img added

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -4,7 +4,7 @@ const AuthImagePattern = ({ title, subtitle }) => {
   return (
     <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
       <div className="max-w-md text-center">
-        <img src="/src/assets/womens.png" alt="" />
+        <img src="https://res.cloudinary.com/dpijljz0p/image/upload/v1742566327/vveoaqbn2vovwrbs64js.png" alt="" />
         <h2 className="text-2xl font-bold mb-4">{title}</h2>
         <p className="text-base-content/60">{subtitle}</p>
       </div>
